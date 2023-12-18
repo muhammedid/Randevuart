d2fde13df1b13243fd32bf687a108cb606b0a181
Muhammed İhsan Dursun <muhammedid@outlook.com>
2023-12-18 22:53:34 +0300

    dataaccess layer

---
+++ DataAccess/Concrete/OnlineRandevuDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class OnlineRandevuDBContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
--- Entities/Concrete/Appointment.cs
+++ Entities/Concrete/Appointment.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace Entities.Concrete
 {
+    //randevu
     public class Appointment
     {
         [Key]
